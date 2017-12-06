7dfc4663b49b21caa426eea47a5aa3b547e54089
Paurav Patel <paurav@users.noreply.github.ccs.neu.edu>
2017-12-06 11:39:45 -0500

    Added model for module

--- server/model/module/module.model.server.js
+++ server/model/module/module.model.server.js
@@ -17,5 +17,54 @@ module.exports = function () {
     model = _model;
   }
 
+  function createModule(_module){
+    return ModuleModel.create(_module);
+  }
 
+  function findModuleById(moduleId) {
+    return ModuleModel.findById(moduleId);
+  }
+
+  function updateModule(moduleId, _module) {
+    return ModuleModel.update({_id: moduleId}, {$set: _module});
+  }
+
+  function recursiveDelete(sectionsOfModule, moduleId) {
+    if (sectionsOfModule.length === 0) {
+      return ModuleModel
+        .remove({_id: moduleId})
+        .then(function (response) {
+          if (response.result.n === 1 && response.result.ok === 1) {
+            return response;
+          }
+        }, function (err) {
+          return err;
+        });
+    }
+
+    return model.sectionModel
+      .deleteSectionAndChildren(sectionsOfModule.shift())
+      .then(function (response) {
+        if (response.result.n === 1 && response.result.ok === 1) {
+          return recursiveDelete(sectionsOfModule, moduleId);
+        }
+      }, function (err) {
+        return err;
+      });
+  }
+
+  function deleteModule(moduleId) {
+    return ModuleModel
+      .findById(moduleId)
+      .then(function(_module) {
+        var sectionsOfModule = _module.sections;
+        return recursiveDelete(sectionsOfModule, moduleId);
+      }, function (err) {
+        return err;
+      });
+  }
+
+  function findAllModule() {
+    return ModuleModel.find({});
+  }
 };
